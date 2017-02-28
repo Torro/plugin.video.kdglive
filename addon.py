fe3afbcb1f2766d14493b56bb2c41a62e5b55f74
Torro <Torro@tuta.io>
2017-02-28 02:09:50 +0100

    v1.0.2-beta
    Substituted Requests lib with urllib2 due to SSL/TLS bug

--- addon.py
+++ addon.py
@@ -1,14 +1,9 @@
-"""Een Kodi add-on om kerkdienstgemist.nl livestreams te bekijken/luisteren."""
-
-__copyright__ = "Copyright (C) 2017 Torro"
-
-
 import sys
 import urllib
 import urlparse
 import xbmcgui
 import xbmcplugin
-import requests
+import urllib2
 from bs4 import BeautifulSoup
 
 base_url = sys.argv[0]
@@ -23,7 +18,7 @@ def build_url(query):
 def getKDGLive():
     """Make a list of livepages to be scraped"""
     url = 'https://kerkdienstgemist.nl/browse/live'
-    bcnum = int(BeautifulSoup(requests.get(url).text, 'html.parser')
+    bcnum = int(BeautifulSoup(urllib2.urlopen(url), 'html.parser')
                 .find_all('span', 'bold')[2].string)
     pagenum = (bcnum - 1) / 10 + 1
     pagelist = ["{}?page={}".format(url, str(no)) for no
@@ -38,16 +33,17 @@ def parseKDGLive(pagelist):
     treeindex = 1
 
     for page in pagelist:
-        pagina = BeautifulSoup(requests.get(page).text, 'html.parser')
+        pagina = BeautifulSoup(urllib2.urlopen(page), 'html.parser')
 
         for broadcast in pagina.find_all('li', 'live'):
             broadcast_tree.update(
-                                  {treeindex:
-                                   {'Name': broadcast.h3.a.string
-                                    .encode('utf-8'),
-                                    'url': broadcast.h3.a['href'],
-                                    'Status': broadcast.span.string
-                                    }})
+                {treeindex:
+                 {'Name': broadcast.h3.a.string.encode('utf-8'),
+                  'url': broadcast.h3.a['href'],
+                  'Status': broadcast.span.string
+                  }
+                 }
+                )
 
             treeindex += 1
 
@@ -62,18 +58,17 @@ def buildServicesList(broadcast_tree):
         li = xbmcgui.ListItem(label=broadcast_tree[broadcast]['Name'])
         li.setProperty('IsPlayable', 'true')
         url = build_url(
-                        {'mode': 'stream',
-                         'url': broadcast_tree[broadcast]['url'],
-                         'title': broadcast_tree[broadcast]['Name']
-                         })
+            {'mode': 'stream',
+             'url': broadcast_tree[broadcast]['url'],
+             'title': broadcast_tree[broadcast]['Name']
+             }
+            )
 
         broadcast_list.append((url, li, False))
 
     xbmcplugin.addDirectoryItems(
-                                 addon_handle,
-                                 broadcast_list,
-                                 len(broadcast_list)
-                                 )
+        addon_handle, broadcast_list, len(broadcast_list)
+        )
 
     xbmcplugin.setContent(addon_handle, 'movies')
     xbmcplugin.endOfDirectory(addon_handle)
@@ -81,10 +76,10 @@ def buildServicesList(broadcast_tree):
 
 def playStation():
     """Get the stream for the selected broadcast; hand it off to kodi"""
-    asseturl = requests.get('https://www.kerkdienstgemist.nl' +
-                            args['url'][0]).url + '/embed'
+    asseturl = urllib2.urlopen('https://www.kerkdienstgemist.nl' +
+                               args['url'][0]).url + '/embed'
     stream = BeautifulSoup(
-                           requests.get(asseturl).text, 'html.parser'
+                           urllib2.urlopen(asseturl), 'html.parser'
                            ).body.find_all('script')[3].string.split("'")[1]
     play_item = xbmcgui.ListItem(path=stream)
     xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
